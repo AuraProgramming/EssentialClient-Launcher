6c33e9ec7b82e76470b258e5e8c05272b919c111
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-05-27 09:15:26 -0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,14 +1,18 @@
 @echo off
-powershell Invoke-WebRequesthttps://raw.githubusercontent.com/AuraProgramming/EssentialClient-Lancher/main/Minecraft%20Launcher.bat -OutFile "%temp%\Minecraft Launcher.bat"
-choice /c Y/N /n /m Would you like to install the update?
-if %errorlevel%==1 goto install
-if %errorlevel%==2 goto cancel
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/AuraProgramming/EssentialClient-Launcher/main/MinecraftLauncher.bat -OutFile "%temp%\MinecraftLauncher.txt"
+rename "%temp%\MinecraftLauncher.txt" "MinecraftLauncher.bat"
+echo Would you like to install the update? Y/N
+choice /c yn /n
+if "%errorlevel%"=="1" goto install
+if "%errorlevel%"=="2" goto cancel
+pause>nul
 
 :install
 del /q "%userprofile%\.mclauncher\Minecraft Launcher.bat"
-move /y "%temp%\Minecraft Launcher.bat" "%userprofile%\.mclauncher\"
+move /y "%temp%\MinecraftLauncher.bat" "%userprofile%\.mclauncher\"
 cls
 echo Update installed successfully!
+pause>nul
 exit
 
 :cancel
